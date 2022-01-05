572aaaead4005dff9020d7b256c3fa1395b9d24d
Elena <ileen.cz@gmail.com>
2022-01-05 13:07:51 +0200

    [feature/create_readme]: Add Homework

---
+++ src/main/java/classroom/PrimitiveTypes.java
@@ -0,0 +1,30 @@
+package classroom;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+// Float Exemple
+        float floatExample = 9999.999f;
+        System.out.println(floatExample);
+        System.out.println(9999.999f);
+
+    double doubleExapmle = 2000000.9000;
+        System.out.println(doubleExapmle);
+        System.out.println(2000000.9000);
+
+// Integral example
+        byte handCount = 2;
+        System.out.println(handCount);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        short distanceFromKievToLatvia = 1011;
+        System.out.println(distanceFromKievToLatvia);
+
+        int populationOfLatvia = 189000;
+        System.out.println(populationOfLatvia);
+
+
+
+    }
+
+}
